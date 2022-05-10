cd663bf0cfb79022502badb6f4db44dbc8a1ddb8
Earl Sabalo <earljohnsabalo@gmail.com>
2022-05-10 11:55:55 +0800

    Update report.php

--- Views/report.php
+++ Views/report.php
@@ -4,6 +4,7 @@ use Classes\Models\ReportModel;
 
 $report_model = new ReportModel();
 
-$report_model->getBest3PointShooters();
+$threept_shooters = $report_model->getBest3PointShooters();
+echo "<h1>Best Three Point Shooter Players</h1>";
+echo asTable($threept_shooters);
 
-//SELECT * from (select 3pt, 3pt_attempted, age from player_totals left join roster on player_totals.player_id = roster.id)  as result;
